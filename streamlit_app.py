f5ceccbeb7b9473f80479ac1add34471cd59f306
Efilym <45981721+Efilym@users.noreply.github.com>
2024-01-14 10:14:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,36 @@
-b = (
-    Bar()
-    .add_xaxis(["Microsoft", "Amazon", "IBM", "Oracle", "Google", "Alibaba"])
-    .add_yaxis("2017-2018 Revenue in (billion $)", random.sample(range(100), 10))
-    .set_global_opts(
-        title_opts=opts.TitleOpts(
-            title="Top cloud providers 2018", subtitle="2017-2018 Revenue"
-        ),
-        toolbox_opts=opts.ToolboxOpts(),
-    )
+import sagemaker
+import boto3
+from sagemaker.huggingface import HuggingFace
+
+try:
+	role = sagemaker.get_execution_role()
+except ValueError:
+	iam = boto3.client('iam')
+	role = iam.get_role(RoleName='sagemaker_execution_role')['Role']['Arn']
+		
+hyperparameters = {
+	'model_name_or_path':'microsoft/phi-2',
+	'output_dir':'/opt/ml/model'
+	# add your remaining hyperparameters
+	# more info here https://github.com/huggingface/transformers/tree/v4.26.0/examples/pytorch/language-modeling
+}
+
+# git configuration to download our fine-tuning script
+git_config = {'repo': 'https://github.com/huggingface/transformers.git','branch': 'v4.26.0'}
+
+# creates Hugging Face estimator
+huggingface_estimator = HuggingFace(
+	entry_point='run_clm.py',
+	source_dir='./examples/pytorch/language-modeling',
+	instance_type='ml.p3.2xlarge',
+	instance_count=1,
+	role=role,
+	git_config=git_config,
+	transformers_version='4.26.0',
+	pytorch_version='1.13.1',
+	py_version='py39',
+	hyperparameters = hyperparameters
 )
-st_pyecharts(
-    b, key="echarts"
-)  # Add key argument to not remount component at every Streamlit run
-st.button("Randomize data")
+
+# starting the train job
+huggingface_estimator.fit()
